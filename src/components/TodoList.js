81a02dce794921660736fd6ecd2c7f02ecb8661e
NatalyGuallichico01 <natilu.naty@gmail.com>
2021-08-23 23:34:19 -0500

    Ejercicio1 Completado (agregar, eliminar, completar Tareas)

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,7 @@
 import { useRef, useState } from "react";
 const TodoList = () => {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(["a", "b", "c"]);
+  const [completed, setCompleted] = useState([]);
   //huks
   const inputRef = useRef(null);
 
@@ -10,12 +11,23 @@ const TodoList = () => {
     //document.querySelector("#todo").value = "";
 
     //huks
+    console.log("input.current", inputRef.current);
     const newTodo = inputRef.current.value;
     setTodos((prevState) => [...prevState, newTodo]);
     inputRef.current.value = "";
   };
   const handleDeleteTask = (index) => {
-    console.log("index", index);
+    console.log(index);
+    const newTodos = todos.filter((todo, i) => i !== index);
+    console.log("NewTodos", newTodos);
+    setTodos(newTodos);
+  };
+  const handleCompleteTask = (positionComplete) => {
+    console.log("positionComplete", positionComplete);
+    const taskComplete = todos[positionComplete];
+    console.log("taskComplete", taskComplete);
+    handleDeleteTask(positionComplete);
+    setCompleted((prevState) => [...prevState, taskComplete]);
   };
   //el ref={inputRef} es parte del huks
   return (
@@ -26,11 +38,13 @@ const TodoList = () => {
         <button onClick={handleAddTask}>Agregar Tarea</button>
       </div>
       <div>
+        <h3>Tareas Pendientes</h3>
         <table>
           <thead>
             <tr>
               <th>Nombre</th>
               <th>Eliminar</th>
+              <th>Completa</th>
             </tr>
           </thead>
           <tbody>
@@ -42,10 +56,23 @@ const TodoList = () => {
                     Eliminar Tarea
                   </button>
                 </td>
+                <td>
+                  <button onClick={() => handleCompleteTask(index)}>
+                    Tarea Completada
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
         </table>
+      </div>
+      <div>
+        <h3>Tareas Completadas</h3>
+        <ul>
+          {completed.map((task, index) => (
+            <li key={index}>{task}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );
